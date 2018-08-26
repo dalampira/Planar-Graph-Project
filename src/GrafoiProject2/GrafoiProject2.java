3a0be22849a270b69a4fe85d026ba1c6e156148f
Sissy Dalampira <sissydk97@gmail.com>
2018-08-26 11:15:16 +0300

    Adding loadMatrxData method and displayTwoDArray method

--- src/GrafoiProject2/GrafoiProject2.java
+++ src/GrafoiProject2/GrafoiProject2.java
@@ -28,8 +28,49 @@ public class GrafoiProject2 {
         int lines = (int) lineCount;
         return lines;
     }
+    /*
+    * This method reads the adjacency matrix from the input file
+    * and stores it to a 2d array that represents the adjacency
+    * matrix to be returned.
+    */
     
+    public static String[][] loadMatrixData(String filename) throws Exception{
+        Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)));
+        int lines = calculateLines(filename);
+        String [][] adjacency_matrix = new String[lines][lines];
+        while(scanner.hasNextLine()) {
+             for (int i=0; i<adjacency_matrix.length; i++) {
+                String[] line = scanner.nextLine().trim().split(" ");
+                for (int j=0; j<line.length; j++) {
+                    adjacency_matrix[i][j] = (String) (line[j]);
+                }
+            }
+        }
+        scanner.close();
+        System.out.println("The adjacency matrix is: ");
+        displayTwoDArray(adjacency_matrix);
+        System.out.println();
+        System.out.println();
+     
+        return adjacency_matrix;
+    }
 
+    /*
+    * This method is used to display the matrices using
+    * 10 digits for every string and having them centered
+    */
+    
+    public static void displayTwoDArray(String[][] matrix){
+        
+        for (String[] row : matrix)
+        {
+            for (String column : row)
+            {
+                System.out.printf("%10s    ", column);
+            }
+        System.out.println();
+        }
+    }
     
     /**
      * @param args the command line arguments
